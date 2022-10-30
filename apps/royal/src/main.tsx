3b8101443f406b9e29277b3ee36e68faba4c9099
ayobna <ayobnas@gmail.com>
2022-10-30 16:44:41 +0200

    Star Wars People in two apps

--- apps/royal/src/main.tsx
+++ apps/royal/src/main.tsx
@@ -3,11 +3,28 @@ import * as ReactDOM from 'react-dom/client';
 
 import App from './app/app';
 
+import { configureStore } from '@reduxjs/toolkit';
+import { Provider } from 'react-redux';
+
+import { REDUX_FEATURE_KEY, reduxReducer } from './app/redux.slice';
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
+const store = configureStore({
+  reducer: { [REDUX_FEATURE_KEY]: reduxReducer },
+  // Additional middleware can be passed to this array
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware(),
+  devTools: process.env.NODE_ENV !== 'production',
+  // Optional Redux store enhancers
+  enhancers: [],
+});
+
 root.render(
-  <StrictMode>
-    <App />
-  </StrictMode>
+  <Provider store={store}>
+    <StrictMode>
+      <App />
+    </StrictMode>
+  </Provider>
 );
